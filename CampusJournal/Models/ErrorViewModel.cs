cbeed95ac6102b7248eae56c0e06a3c5a2e2d47f
vertio <krzysztofdrzewiecki690@gmail.com>
2023-05-02 18:34:58 +0200

    Dodaj pliki projektów.

---
+++ CampusJournal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CampusJournal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
